19ea5d793cf1ffaff2d86d572e6746429ffcb4b1
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-12 17:26:19 -0500

    Add index route to app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.locals.pluralize = require('pluralize');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+// Index route
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
