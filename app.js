4aabd847ecbef5080a156147d73cf09de2f6a7eb
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-12 17:32:24 -0500

    Add authentication routes to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -26,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // Index route
+
+app.use('/', indexRouter);
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
